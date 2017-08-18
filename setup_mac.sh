d0cfa90b77d9e683eeafe1c58d8d53c1471c29cc
Vivek Kumar <vivekk.chugh@gmail.com>
2017-08-18 00:03:26 -0700

    updated mac scropt

--- setup_mac.sh
+++ setup_mac.sh
@@ -1,21 +1,19 @@
-#!/bin/bash
-# Simple setup.sh for configuring on MAC
-# for headless setup. 
-cd $HOME
-echo "Please make sure node.js and npm are installedhttp://nodejs.org/download"
+## Install Brew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew install wget
+brew install bash-completion 
+brew install wget 
+brew install ffmpeg 
+brew install imagemagick
+brew install node
+brew install rlwrap
 
-## curl https://raw.github.com/creationix/nvm/master/install.sh | sh
-## # Load nvm and install latest production node
-## source $HOME/.nvm/nvm.sh
-## nvm install v0.10.12
-## nvm use v0.10.12
-
-
+# get and clone the dotfiles
 git clone https://github.com/vzk/dotfiles.git
-ln -sb dotfiles/.bash_profile .
-ln -sb dotfiles/.bashrc .
-ln -sb dotfiles/.bashrc_custom .
-ln -sb dotfiles/.vimrc .
+ln -s dotfiles/.bash_profile .
+ln -s dotfiles/.bashrc .
+ln -s dotfiles/.bashrc_custom .
+ln -s dotfiles/.vimrc .
 
 ## setup VIM
 
@@ -27,11 +25,3 @@ curl https://raw.githubusercontent.com/Lokaltog/vim-distinguished/develop/colors
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 sudo npm install -g jshint # Install link for javascript
 vim +PluginInstall +qall
-
-
-
-## setup commonly used components for javascript development
-sudo npm install -g connect
-sudo npm install -g serve-static #for setting up a  server
-
-# node paths are installed in the following directory - they may note be always included
